2a8e2953e4464b8b685b4bde80b2ffece6c26237
godhanidenis <godhanidenis@gmail.com>
2024-03-12 17:04:43 +0530

    Create Custom Element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [AppComponent, HelloComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [HelloComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const widgetElement = createCustomElement(HelloComponent, {
+      injector: this.injector,
+    });
+    customElements.define('hello-widget', widgetElement);
+  }
+}
