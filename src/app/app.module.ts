31e6f48e8fe5e7d0df6ae9703dece4c69950ee76
godhanidenis <godhanidenis@gmail.com>
2024-03-14 10:25:40 +0530

    Remove all unnacessary code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,14 @@
-import { Injector, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
-import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [AppComponent, HelloComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-  constructor(private injector: Injector) {}
-
-  ngDoBootstrap() {
-    const widgetElement = createCustomElement(HelloComponent, {
-      injector: this.injector,
-    });
-    customElements.define('hello-widget', widgetElement);
-  }
-}
+export class AppModule {}
